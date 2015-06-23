5cb0348efa343d1dd0716f75c535bba6004414ff
daniel-llach <daniel.llach@gmail.com>
2015-06-23 18:46:51 -0300

    pro maintainer

--- js/maintainer.js
+++ js/maintainer.js
@@ -2,11 +2,13 @@ define([
 	"backbone.marionette",
 	"backbone.radio",
 	"radio.shim",
+    "../assets/menu/js/menu",
+    "../assets/appbar/js/appbar",
     "text!templates/maintainer.html",
     "text!templates/modes.html",
     "text!regions/templates/fullpage.html",
     "text!regions/templates/columnHeaderMainflip.html"
-], function (Marionette, Radio, Shim, TemplateMaintainer, TemplateModes, FullPageTemplate, ColumnHeaderMainflip) {
+], function (Marionette, Radio, Shim, Menu, AppBar, TemplateMaintainer, TemplateModes, FullPageTemplate, ColumnHeaderMainflip) {
     // Enviroment
     var App = new Marionette.Application();
 
@@ -17,9 +19,32 @@ define([
         className: "component"
     });
 
+    // Main menu region
     var Target = new ExampleLayout();
     Target.addRegion("main", "#somediv");
+    var menuregion = Marionette.Region.extend({
+        el: "#menucontainer"
+    });
+    Target.addRegion("mainMenu", new menuregion());
     Target.render();
+
+    // Appbar region
+    var Target2 = new ExampleLayout();
+    Target2.addRegion("headerRight", "header .right");
+    var appBarregion = Marionette.Region.extend({
+        el: "header .right"
+    });
+    Target2.addRegion("appbar", new appBarregion());
+    Target2.render();
+
+    // Outer region
+    var Target3 = new ExampleLayout();
+    Target3.addRegion("outer", ".outer");
+    var outerRegion = Marionette.Region.extend({
+        el: ".outer"
+    });
+    Target3.addRegion("outer", new outerRegion());
+    Target3.render();
 
     // Maintainer Module!
     App.module("Maintainer", function(Maintainer, App){
@@ -68,7 +93,8 @@ define([
             regions: {
                 headerLeft: ".header .left",
                 headerRight: ".header .right",
-                container: ".container"
+                container: ".container",
+                outer: ".outer"
             },
 
             events: {
@@ -257,6 +283,7 @@ define([
 
         Target.main.show(maintainerView);
 
+
         App.Channel.on("change:layout", function(args){
             maintainerChannel.command("set:layout", args);
         });
@@ -264,6 +291,185 @@ define([
         App.Channel.listenTo(maintainerChannel, "action:button:click", function(){
             alert("click action desactivado (no modify)");
         });
+
+
+
+
+
+
+        // show main menu
+        // data items
+        var dataitems = [
+            {
+                nombre: "infraestructura",
+                icon: "infraestructura",
+                subitems: [
+                    {
+                        nombre: "sedes",
+                        link: "#"
+                    },
+                    {
+                        nombre: "edificios",
+                        link: "#"
+                    },
+                    {
+                        nombre: "salas",
+                        link: "#"
+                    }
+                ]
+            },
+            {
+                nombre: "institución",
+                icon: "institucion",
+                subitems: [
+                    {
+                        nombre: "facultades",
+                        link: "#"
+                    },
+                    {
+                        nombre: "escuelas",
+                        link: "#"
+                    },
+                    {
+                        nombre: "regímenes",
+                        link: "#"
+                    },
+                    {
+                        nombre: "carreras",
+                        link: "#"
+                    }
+                ]
+            },
+            {
+                nombre: "agenda",
+                icon: "agenda",
+                subitems: [
+                    {
+                        nombre: "periodos",
+                        link: "#"
+                    },
+                    {
+                        nombre: "semanas",
+                        link: "#"
+                    },
+                    {
+                        nombre: "bloques",
+                        link: "#"
+                    }
+                ]
+            },
+            {
+                nombre: "programación",
+                icon: "programacion",
+                subitems: [
+                    {
+                        nombre: "asignaturas",
+                        link: "#"
+                    },
+                    {
+                        nombre: "demandas",
+                        link: "https://www.youtube.com/watch?v=oHg5SJYRHA0"
+                    }
+                ]
+            },
+            {
+                nombre: "planta docente",
+                icon: "profesor",
+                subitems: [
+                    {
+                        nombre: "profesores",
+                        link: "#"
+                    },
+                    {
+                        nombre: "oferta de profesores",
+                        link: "#"
+                    }
+                ]
+            }
+        ]
+
+        // start menu
+        var menu = new Menu("menu");
+        var menuChannel = menu.Channel;
+        menu.start({
+            items : dataitems  
+        });
+        var menuView = menuChannel.request("get:root");
+        Target.getRegion("mainMenu").show(menuView);
+
+
+        // show Appbar
+        // json scenarios
+        var scenarios = [{"id":5,"environment":"planner","database":"darwined_siglo21_planner_5","name":"20151 - SC","comment":"Escenario Base con Sede \u00danica","active":false,"enabled":true,"created":"2014-11-05 09:52:17","modified":"2014-11-05 09:52:31"},{"id":6,"environment":"planner","database":"ko","name":"esc2","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":7,"environment":"planner","database":"ko","name":"esc2","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":8,"environment":"planner","database":"ko","name":"esc3","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":9,"environment":"planner","database":"ko","name":"esc4","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":10,"environment":"planner","database":"ko","name":"esc5","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":11,"environment":"planner","database":"ko","name":"esc6","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":12,"environment":"planner","database":"ko","name":"esc7","comment":"otro","active":false,"enabled":true,"created":"2014-12-17 00:00:00","modified":"2014-12-17 00:00:00"},{"id":13,"environment":"planner","database":"darwined_uniminuto_planner_23","name":"UNIMINUTO","comment":null,"active":false,"enabled":true,"created":"2015-02-19 00:00:00","modified":"2015-02-20 00:00:00"},{"id":14,"environment":"planner","database":"darwined_aiep_barriouniversitario_10","name":"AIEP","comment":"jojo","active":false,"enabled":true,"created":"2015-03-20 00:00:00","modified":"2015-03-27 00:00:00"},{"id":15,"environment":"planner","database":"darwined_fuaa_medellin_43","name":"fuaa medellin","comment":null,"active":false,"enabled":true,"created":"2015-02-19 00:00:00","modified":"2015-02-20 00:00:00"},{"id":16,"environment":"planner","database":"darwined_upn_planner_2","name":"upn 2 2015","comment":"domingos proceso 32","active":true,"enabled":true,"created":"2015-03-20 00:00:00","modified":"2015-03-27 00:00:00"}]
+        var active_scenario = _.where(scenarios, {active: true});
+
+        // data
+        var dataAppbar = [
+            {
+                nombre: "escenarios",
+                active_scenario: active_scenario[0].name,
+                template: "escenarios",
+                modal: {
+                    asset: "selector",
+                    template: "escenarios",
+                    scenarios: scenarios
+                }
+            },
+            {
+                nombre: "menuapps",
+                icon: "menuapps",
+                template: "menuapps",
+                modal: {
+                    asset: "listItems",
+                    template: "menuapps",
+                    apps: [
+                        {
+                            nombre: "mantenedores",
+                            active: true,
+                            icon: "mantenedores"
+                        },
+                        {
+                            nombre: "procesos",
+                            active: false,
+                            icon: "procesos"
+                        }
+                    ]
+                }
+            },
+            {
+                nombre: "messages",
+                icon: "messages",
+                template: "messages",
+                modal: {
+                    asset: "listItems",
+                    template: "messages",
+                    messages: [
+                        {
+                            id: "r654r",
+                            content: "mensaje1",
+                            type: "message",
+                            from: "user12"
+                        },
+                        {
+                            id: "r654r",
+                            content: "mensaje2",
+                            type: "warning",
+                            from: "sistem"
+                        }
+                    ]
+                }
+            }
+        ];
+
+        // start appbar
+        var appbar = new AppBar("appbar");
+        var appbarChannel = appbar.Channel;
+        appbar.start({
+            items : dataAppbar,
+            region: Target3.getRegion("outer")
+        });
+        var appbarView = appbarChannel.request("get:root");
+        Target2.getRegion("appbar").show(appbarView);
     });
 
     return App;
